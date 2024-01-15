f3e5dde9f3fd8cba5bcee01b1e63186bba0cc6f4
fanlingdong <z5295470@ad.unsw.edu.au>
2024-01-15 14:32:16 +1100

    c_sharp test

---
+++ Algorithm_CSharp/Leetcode/105. Construct Binary Tree from Preorder and Inorder Traversal.cs
@@ -0,0 +1,24 @@
+namespace DefaultNamespace;
+
+public class 105__Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal {
+    
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode BuildTree(int[] preorder, int[] inorder) {
+        
+    }
+}
