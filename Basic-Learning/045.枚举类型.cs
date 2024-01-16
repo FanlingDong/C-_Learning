a04e91bdca264319404d3d50b2c6a00e379925ee
fanlingdong <z5295470@ad.unsw.edu.au>
2024-01-16 23:54:16 +1100

    c#结构体

---
+++ Basic-Learning/045.枚举类型.cs
@@ -0,0 +1,5 @@
+namespace DefaultNamespace;
+
+public class 045_枚举类型 {
+    
+}
---
+++ Basic-Learning/046.结构体.cs
@@ -0,0 +1,12 @@
+namespace DefaultNamespace;
+// 结构体就是把某一类信息进行整合，组成一个新的数据类型，相当于一个新的信息
+public class 046_结构体 {
+    static void Main(string[] args)
+    {
+        struct <name>
+        {
+            访问权限 type typename;
+            访问权限 type typename;
+        }
+    }
+}
